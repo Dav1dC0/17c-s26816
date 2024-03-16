e0132e2c776e7ccc10796664318f82ab47077ff5
Dav1dC0 <161834741+Dav1dC0@users.noreply.github.com>
2024-03-16 22:27:57 +0200

    Task 2 commit

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-squares = [x**2 for x in range(1, 11)]
-print(squares)
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        return [x**2 for x in range(start, end + 1)]
+
+square_gen = SquareGenerator()
+example_squares = square_gen.generate_squares(1, 10)
+print(example_squares)
